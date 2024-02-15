c6cff02ff11cbdadf5c08fd8bfa83a2209324f86
Filippo Ferrario <filippo.f3rrario@gmail.com>
2024-02-14 22:07:40 -0500

    added main files in telemetry and environmental folders

---
+++ preprocessing-environmental_data/R_workflow/0-main-environmental.R
@@ -0,0 +1,17 @@
+# ===============================================================================
+# Name   	: Main environmental data
+# Author 	: Filippo Ferrario
+# Date   	:  [dd-mm-yyyy] 14-02-2024
+# Version	: 1
+# URL		: 
+# Aim    	: 
+# ===============================================================================
+
+setwd('TEST-collaboration')
+
+# run this section only the first time.
+dir.create('R_output/dataset', showWarnings = TRUE, recursive = T)
+dir.create('R_output/images', showWarnings = TRUE, recursive = T)
+dir.create('R_output/Rmarkdown', showWarnings = TRUE, recursive = T)
+dir.create('R_output/spatial', showWarnings = TRUE, recursive = T)
+ 
