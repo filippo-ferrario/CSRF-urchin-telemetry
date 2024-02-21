54bae635649dddfacf8a5b9c032b902e2646d6c8
Ferrario <filippo.f3rrario@gmail.com>
2024-02-21 15:01:25 -0500

    editet JS and MFL main files to refer to the correct script

--- TEST-collaboration/R_workflow/MFL-0-main-telemBC.R
+++ TEST-collaboration/R_workflow/MFL-0-main-telemBC.R
@@ -44,5 +44,5 @@ library(ezknitr)
 # example on how parallize you code
 # -----------------------------------
 # input: none
-ezspin(file='R_workflow/FF-example-parallel_processing.R', keep_md=FALSE,  out_dir='R_output/Rmarkdown')
+ezspin(file='R_workflow/MFL-example-parallel_processing.R', keep_md=FALSE,  out_dir='R_output/Rmarkdown')
 # input: /R_output/Rmarkdown/FF-example-parallel_processing.html
