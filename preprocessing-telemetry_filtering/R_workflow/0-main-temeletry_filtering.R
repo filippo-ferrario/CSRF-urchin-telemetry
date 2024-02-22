451a95afffb201002918de34ebf6f8ffea10a2fc
Ferrario <filippo.f3rrario@gmail.com>
2024-02-22 12:49:58 -0500

    corrected Work directory for subprojects

--- preprocessing-telemetry_filtering/R_workflow/0-main-temeletry_filtering.R
+++ preprocessing-telemetry_filtering/R_workflow/0-main-temeletry_filtering.R
@@ -7,7 +7,7 @@
 # Aim    	: 
 # ===============================================================================
 
-setwd('TEST-collaboration')
+setwd('preprocessing-telemetry_filtering')
 
 # run this section only the first time.
 dir.create('R_output/dataset', showWarnings = TRUE, recursive = T)
