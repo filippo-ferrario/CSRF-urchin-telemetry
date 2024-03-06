aef0df4030ee9c315e8c854dd25e4aa4cbe33ae7
Filippo Ferrario <filippo.f3rrario@gmail.com>
2024-03-06 17:28:29 -0500

    reworked the forlder hierarchy
    reworked the forlder hierarchy  to set upe real work.

---
+++ R_workflow/00-main.R
@@ -0,0 +1,18 @@
+# ===============================================================================
+# Name   	: Master Main file
+# Author 	: Filippo Ferrario
+# Date   	: 2024-03-06
+# Version	: 
+# URL		: 
+# Aim    	: contains all the instructions for the complete project.
+# ===============================================================================
+
+
+# run this section only the first time.
+dir.create('R_output/dataset', showWarnings = TRUE, recursive = T) # This folder is to store datasets and their metadata,
+dir.create('R_output/plots', showWarnings = TRUE, recursive = T) # This folder is to store plots from analysis 
+dir.create('R_output/Rmarkdown', showWarnings = TRUE, recursive = T) # This folder is to store Markdowns and htmls
+dir.create('R_output/raster_vector_data', showWarnings = TRUE, recursive = T) # This folder is to store spatial data and their metadata in formats like:
+dir.create('R_output/R_objects', showWarnings = TRUE, recursive = T) # This folder is to store the R objects and their metadata.
+ 
+
---
+++ R_workflow/preprocessing-telemetry_filtering/0-main-temeletry_filtering.R
@@ -0,0 +1,10 @@
+# ===============================================================================
+# Name   	: Main telemetry filtering
+# Author 	: Filippo Ferrario
+# Date   	:  [dd-mm-yyyy] 14-02-2024
+# Version	: 1
+# URL		: 
+# Aim    	: 
+# ===============================================================================
+
+# ATTENTION: remember to reflect the path of the script in the source function
