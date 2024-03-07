dcf6bbd703d77e06aceb50765b770add00e64e13
Filippo Ferrario <filippo.f3rrario@gmail.com>
2024-03-07 12:20:18 -0500

    began creating data folder hiearchy

--- R_workflow/00-main.R
+++ R_workflow/00-main.R
@@ -8,6 +8,17 @@
 # ===============================================================================
 
 
+
+# create hierarchy for raw data folder
+# =======================================
+
+dir.create('data/raw_data_downloads_bic_2022/vps_results_Bic_2022/La_Baleine/results/', showWarnings = TRUE, recursive = T)
+
+
+
+# create hierarchy for R_output folder
+# =======================================
+
 # run this section only the first time.
 dir.create('R_output/dataset', showWarnings = TRUE, recursive = T) # This folder is to store datasets and their metadata,
 dir.create('R_output/plots', showWarnings = TRUE, recursive = T) # This folder is to store plots from analysis 
