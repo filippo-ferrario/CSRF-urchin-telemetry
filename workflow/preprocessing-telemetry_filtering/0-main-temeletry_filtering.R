b97a9470160729f87cf55c968ca9be5b161ca42f
Lavoie <Marie-France.Lavoie@dfo-mpo.gc.ca>
2025-01-30 14:48:47 -0500

    Path changed for source() color in main prepocessing-telemetry_filtering

--- workflow/preprocessing-telemetry_filtering/0-main-temeletry_filtering.R
+++ workflow/preprocessing-telemetry_filtering/0-main-temeletry_filtering.R
@@ -3,7 +3,7 @@
 # Author 	: Katie MacGregor
 # Date   	:  [yyyy-mm-dd] 2025-01-27
 # Version	: 1
-# URL		: 
+# URL		:
 # Aim    	: Organize the process of prepping the telemetry data sets
 # ===============================================================================
 
@@ -18,7 +18,7 @@
 # =========================
 # load packages
 # =========================
-# library(shaRe) # to facilitate data and metadata export 
+# library(shaRe) # to facilitate data and metadata export
 library(ezknitr)
 
 library(RColorBrewer)
@@ -50,7 +50,7 @@ conflicted::conflicts_prefer(dplyr::select)
 
 # Load colour schemes for plotting
 # ---------------------------------------------
-source('./workflow/00_colourPalettes.R')
+source('./workflow/preprocessing-telemetry_filtering/00_colourPalettes.R')
 
 
 
@@ -65,32 +65,32 @@ source('./workflow/00_colourPalettes.R')
 # ---------------------------------------------
 # input:./data/metadata_urchins_QC2022.csv
 # input: ./data/metadata_receivers_QC2022.csv
-# input: 
-# input: 
-# Bic: Anse des Pilotes                                                                                                                                                                                             
+# input:
+# input:
+# Bic: Anse des Pilotes
 # input: ./data/raw_data_downloads_bic_2022/vps_results_bic_2022/anse_des_pilotes/results/animal/all.csv
 # input: ./data/raw_data_downloads_bic_2022/vps_results_bic_2022/anse_des_pilotes/results/syncref/all.csv
 # Bic: Baleines
-# input: 
-# input:  
+# input:
+# input:
 # Iles aux Lievres - southwest
-# input: 
-# input: 
+# input:
+# input:
 # Iles aux Lievres - northeast
-# input: 
-# input: 
+# input:
+# input:
 # Prince Rupert - Tugwell 1
-# input: 
-# input: 
+# input:
+# input:
 # Prince Rupert - Tugwell 2
-# input: 
-# input: 
+# input:
+# input:
 # Quadra - Marina 1
-# input: 
-# input: 
+# input:
+# input:
 # Quadra - Marina 2
-# input: 
-# input:   
+# input:
+# input:
 
 source('./workflow/preprocessing-telemetry_filtering/import_combine_and_preprocess_HR2_telemetry_data.R'); rm(list=ls())
 
@@ -144,7 +144,7 @@ ezspin(file = './workflow/explore_and_calculate_median_positions_from_vps_result
 source('R_workflow/filter_and_plot_urchin_detection_data.R'); rm(list=ls())
 # output: ./R_output/plots/animal_HPE_q90_by_urchin.png
 # output: ./R_output/plots/animal_HPE_q90_by_month.png
-# output: ./R_output/plots/animal_RMSE_q90_by_urchin.png 
+# output: ./R_output/plots/animal_RMSE_q90_by_urchin.png
 # output: ./R_output/plots/animal_RMSE_q90_by_month.png
 # output: ./R_output/plots/urchin_detections/animal_detections_1week_test.png
 # output: ./R_output/datasets/one_individual_test_data_for_filtering.csv
@@ -170,7 +170,7 @@ ezspin(file = 'R_workflow/filter_and_plot_urchin_detection_data.R', keep_md = FA
 # input: ./R_output/datasets/one_individual_test_data_for_filtering.csv
 # input: ./data/site_polygon.gpkg
 source('R_workflow/filtering_strategy_HR_urchin_data_test_with_small_dataset.R'); rm(list=ls())
-# output: 
+# output:
 
 
 
