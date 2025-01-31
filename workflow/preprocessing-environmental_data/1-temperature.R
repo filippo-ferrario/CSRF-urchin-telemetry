d3a16d78f11644a6df6a206ec59de99cceb432f2
jjshao <jillian.j.shao@gmail.com>
2025-01-31 12:02:07 -0500

    Renaming objects and adding time for filtering

--- workflow/preprocessing-environmental_data/1-temperature.R
+++ workflow/preprocessing-environmental_data/1-temperature.R
@@ -36,15 +36,35 @@ setwd("C:/Users/shaoj/Documents/csrf_urchin/qc/")
 
 metadata <- read.csv("./metadata_sensors_QC2022.csv")
 
+# Function to convert time strings
+convert_time <- function(time_str) {
+  if (time_str == "") {
+    return("")
+  }
+
+  # Correct time zone and extract time
+  time_str <- gsub("Hest", "", time_str)
+  time_str <- trimws(time_str)
+  time_str <- paste0(time_str, ":00")
+
+  return(time_str)
+}
+
+# Apply conversion to the column
+metadata$Time_deployed_POSIX <- sapply(metadata$Time_deployed_UTC, convert_time)
+
+# Create a new datetime column
+metadata$DateTime_deployed <- as.POSIXct(paste(metadata$Date_deployed, metadata$Time_deployed_POSIX), format = "%Y-%m-%d %H:%M:%S")
+
 #####################
 #### TCM devices ####
 #####################
-all.csv.files <- list.files(path = ".", pattern = "\\_Temperature.csv", recursive = TRUE,
-                            ignore.case = TRUE, include.dirs = TRUE)
+all.csv.tcm <- list.files(path = ".", pattern = "\\_Temperature.csv", recursive = TRUE,
+                          ignore.case = TRUE, include.dirs = TRUE)
 
 # Load all data
-all.data <- data.frame()
-for (file in all.csv.files) {
+all.data.tcm <- data.frame()
+for (file in all.csv.tcm) {
   data <- read.csv(file)
 
   ## Note: For Quebec TCM (sensory ID is in the file name)
@@ -57,23 +77,26 @@ for (file in all.csv.files) {
   # Merge with metadata
   data <- merge(metadata, data, by = "Sensor_ID", all.y = TRUE)
 
-  # Append to all.data
-  if (nrow(all.data) == 0) {
-    nrowbefore <- nrow(all.data)
-    all.data <- data
+  # Append to all.data.tcm
+  if (nrow(all.data.tcm) == 0) {
+    nrowbefore <- nrow(all.data.tcm)
+    all.data.tcm <- data
   } else {
-    nrowbefore <- nrow(all.data)
-    all.data <- rbind(all.data, data)
+    nrowbefore <- nrow(all.data.tcm)
+    all.data.tcm <- rbind(all.data.tcm, data)
   }
 
   # Print check
-  print(paste("Added right number of rows: ", nrow(all.data) - nrowbefore == nrow(data)))
+  print(paste("Added right number of rows: ", nrow(all.data.tcm) - nrowbefore == nrow(data)))
 }
 
 # Change Time column from displaying "2022-07-29T00:00:00.000" to "2022-07-29 00:00:00.000"
-all.data$DateTime <- as.POSIXct(gsub("T", " ", all.data$ISO.8601.Time))
+all.data.tcm$DateTime <- as.POSIXct(gsub("T", " ", all.data.tcm$ISO.8601.Time))
 # 724091 points
 
+# Remove points from outside deployment time
+all.data.tcm.cleaned <- subset(all.data.tcm,
+                               all.data.tcm$DateTime > all.data.tcm$Date_deployed & all.data.tcm$Date_recovered > all.data.tcm$DateTime)
 
 #############################
 #### AquaMeasure devices ####
@@ -104,7 +127,12 @@ for (file in all.csv.am) {
   print(paste("Added right number of rows: ", nrow(all.data.am) - nrowbefore == nrow(data)))
 }
 
-# Remove points between time
+# Make sure column with data point date/time are in POSIX tz = UTC
+all.data.am$DateTime <- as.POSIXct(all.data.am$Time..UTC., tz = "UTC")
+
+# Remove points from outside deployment time
+all.data.am.cleaned <- all.data.am[(is.na(all.data.am$DateTime_deployed) & all.data.am$DateTime > all.data.am$Date_deployed & all.data.am$DateTime < all.data.am$Date_recovered) |
+                  (!is.na(all.data.am$DateTime_deployed) & all.data.am$DateTime > all.data.am$DateTime_deployed & all.data.am$DateTime < all.data.am$Date_recovered), ]
 
 
 ##########################
@@ -112,28 +140,17 @@ for (file in all.csv.am) {
 ##########################
 
 # Rename columns to match
-names(all.data)[names(all.data) == 'Temperature..C.'] <- 'Temperature'
-names(all.data.am)[names(all.data.am) == 'Temperature..degC.'] <- 'Temperature'
-names(all.data.am)[names(all.data.am) == 'Time..UTC.'] <- 'DateTime'
+names(all.data.tcm.cleaned)[names(all.data.tcm.cleaned) == 'Temperature..C.'] <- 'Temperature'
+names(all.data.am.cleaned)[names(all.data.am.cleaned) == 'Temperature..degC.'] <- 'Temperature'
+names(all.data.am.cleaned)[names(all.data.am.cleaned) == 'Time..UTC.'] <- 'DateTime'
 
 # Keep only some columns
-col.keep <- c("Sensor_ID", "Type", "Site", "Position", "Date_deployed", "Time_deployed_UTC",
-              "Date_recovered", "DateTime", "Temperature")
-all.data <- subset(all.data, select = col.keep)
-all.data.am <- subset(all.data.am, select = col.keep)
-
-# Make sure all columns with date/time are in POSIX tz = UTC
-all.data.am$DateTime <- as.POSIXct(all.data.am$DateTime, tz = "UTC")
+col.keep <- c("Sensor_ID", "Type", "Site", "Position", "DateTime", "Temperature")
+all.data.tcm.cleaned <- subset(all.data.tcm.cleaned, select = col.keep)
+all.data.am.cleaned <- subset(all.data.am.cleaned, select = col.keep)
 
 # Merge to all.data
-all.data.merged <- rbind(all.data, all.data.am)
-all.data.merged$Date_deployed <- as.POSIXct(all.data.merged$Date_deployed, format = "%Y-%m-%d")
-all.data.merged$Date_recovered <- as.POSIXct(all.data.merged$Date_recovered, format = "%Y-%m-%d")
-
-### NEED TO SWITCH TO INDIVIDUAL DEVICES
-# Remove points from outside deployment time
-all.data.cleaned <- subset(all.data.merged,
-                           all.data.merged$DateTime > all.data.merged$Date_deployed & all.data.merged$Date_recovered > all.data.merged$DateTime)
+all.data.merged <- rbind(all.data.tcm.cleaned, all.data.am.cleaned)
 
 # Are they all unique DateTime + Site?
 #print(nrow(unique(all.data.cleaned[, c("DateTime", "Site")])) == nrow(all.data.cleaned))
@@ -141,11 +158,11 @@ all.data.cleaned <- subset(all.data.merged,
 
 #### Plotting ####
 # Split the data by Site
-all.data.cleaned <- split(all.data.cleaned, all.data.cleaned$Site)
+all.data.merged <- split(all.data.merged, all.data.merged$Site)
 
 # Plot by site
 # Determine the number of dataframes
-n_plots <- length(all.data.cleaned)
+n_plots <- length(all.data.merged)
 
 # Calculate the number of rows and columns for the plot grid
 n_cols <- ceiling(sqrt(n_plots))
@@ -155,7 +172,7 @@ n_rows <- ceiling(n_plots / n_cols)
 par(mfrow = c(n_rows, n_cols))
 
 # Plot each dataframe
-for (df in all.data.cleaned) {
+for (df in all.data.merged) {
   plot(df$DateTime, df$Temperature,
        xlab = "Date", ylab = "Temperature",
        main = paste("Site:", unique(df$Site)))
