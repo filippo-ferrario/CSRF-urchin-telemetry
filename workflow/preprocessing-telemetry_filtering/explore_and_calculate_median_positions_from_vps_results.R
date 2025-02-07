3ec82f0a74d3d3fd833e143a500825277bd4e0eb
Lavoie <Marie-France.Lavoie@dfo-mpo.gc.ca>
2025-02-07 15:27:07 -0500

    Update the explore_and_calculate_median script
    - create a new graph with the median and receivers and VPS positions (ggplot)
    - Calculate the distance between the median, receivers, and VPS coordinates
    - See the density points on a plot

--- workflow/preprocessing-telemetry_filtering/explore_and_calculate_median_positions_from_vps_results.R
+++ workflow/preprocessing-telemetry_filtering/explore_and_calculate_median_positions_from_vps_results.R
@@ -2,16 +2,16 @@
 # Name   	: Plot, explore and calculate median positions from vps data for photomosaic building
 # Author 	: Katie MacGregor
 # Date   	: 2024-01-27 [yyyy-mm-dd]
-# Version	: 
-# URL		: 
+# Version	:
+# URL		:
 # Aim    	: Explore and extract median gps positions for photomosaic
 # ===============================================================================
 
 
 #' # Explore and calulate median positions from vps
 #' ----------------------------------------------------
-#' 
-#' Plot and explore 
+#'
+#' Plot and explore
 
 
 
@@ -25,6 +25,16 @@ sync_data <- readr::read_csv("./output/datasets/combined_sync_and_ref_data.csv",
 
 glimpse(sync_data)
 
+# Import receivers metadata
+rece <- read.csv("./output/datasets/receivers_metadata_QC_BC.csv", sep = ",")
+# filter to only have the Pilotes receivers data
+rece_pilotes <- rece %>% filter (Site_2 == "Pilotes")
+
+# Import adjusted receivers coordinates from VPS
+vps_rece <- read.csv("./output/datasets/VPS_receivers_metadata_QC_BC.csv", sep = ",")
+vps_pilotes <- vps_rece %>% filter (Site_2 == "Pilotes")
+
+
 
 
 #' # Distribution of HPE and RMSE values in filtered dataset
@@ -35,14 +45,14 @@ glimpse(sync_data)
 # =====================
 
 #' HPE
-# All stationary tags have the vast majority (95% overall) of their detections with HPE < 2 
+# All stationary tags have the vast majority (95% overall) of their detections with HPE < 2
 # (and generally less than 1).
 quantile(sync_data$hpe, c(0.5, 0.9, 0.95, 0.99), na.rm = TRUE)
 
-#' Visualize the distribution of hpe values (after filtering by the 95% quantile to remove extreme values) 
+#' Visualize the distribution of hpe values (after filtering by the 95% quantile to remove extreme values)
 fltr_hpe95 <- quantile(sync_data$hpe, c(0.95), na.rm = TRUE)
-sync_data |> 
-  filter(hpe < fltr_hpe95)  |> 
+sync_data |>
+  filter(hpe < fltr_hpe95)  |>
   ggplot(aes(x = hpe)) +
   geom_density(outline.type = "full", fill = cbbPalette[3], alpha = 0.6) +
   labs(title = "HPE for Sync and Ref tags (filtered by 95% quantile)", subtitle = paste(unique(sync_data$site))) +
@@ -51,8 +61,8 @@ sync_data |>
 
 #' We can filter much more aggressively, though, say by 50%:
 fltr_hpe50 <- quantile(sync_data$hpe, c(0.50), na.rm = TRUE)
-sync_data |> 
-  filter(hpe < fltr_hpe50)  |> 
+sync_data |>
+  filter(hpe < fltr_hpe50)  |>
   ggplot(aes(x = hpe)) +
   geom_density(outline.type = "full", fill = cbbPalette[3], alpha = 0.6) +
   labs(title = "HPE for Sync and Ref tags (filtered by 50% quantile)", subtitle = paste(unique(sync_data$site))) +
@@ -61,8 +71,8 @@ sync_data |>
 
 
 
-p1 <- sync_data |>  
-  dplyr::filter(hpe < fltr_hpe50) |>  
+p1 <- sync_data |>
+  dplyr::filter(hpe < fltr_hpe50) |>
   ggplot(aes(x = hpe)) +
   geom_density(outline.type = "full", fill = cbbPalette[3], alpha = 0.6) +
   labs(title = "HPE for Sync and Ref tags (filtered by 50% quantile)", subtitle = paste(unique(sync_data$site))) +
@@ -71,8 +81,8 @@ p1 <- sync_data |>
 p1
 ggsave(p1, file = "./output/plots/HPE_q50_by_station.png", width = 8, height = 8)
 
-p2 <- sync_data  |>  
-  dplyr::filter(hpe < fltr_hpe50) |>  
+p2 <- sync_data  |>
+  dplyr::filter(hpe < fltr_hpe50) |>
   ggplot(aes(x = hpe)) +
   geom_density(outline.type = "full", fill = cbbPalette[3], alpha = 0.6) +
   labs(title = "HPE for Sync and Ref tags (filtered by 50% quantile)", subtitle = paste(unique(sync_data$site))) +
@@ -88,8 +98,8 @@ ggsave(p2, file = "./output/plots/HPE_q50_by_month.png", width = 8, height = 8)
 quantile(sync_data$rmse, c(0.5, 0.9, 0.95, 0.99), na.rm = TRUE)
 fltr_rmse50 <- quantile(sync_data$rmse, c(0.5), na.rm = TRUE)
 
-p3 <- sync_data %>% 
-  dplyr::filter(rmse < fltr_rmse50)  %>% 
+p3 <- sync_data %>%
+  dplyr::filter(rmse < fltr_rmse50)  %>%
   ggplot(aes(x = rmse)) +
   geom_density(outline.type = "full", fill = cbbPalette[3], alpha = 0.6) +
   labs(title = "RMSE for Sync and Ref tags (filtered by 50% quantile)", subtitle = paste(unique(sync_data$site))) +
@@ -97,8 +107,8 @@ p3 <- sync_data %>%
 p3
 ggsave(p3, file = "./output/plots/RMSE_q50_by_station.png", width = 8, height = 8)
 
-p4 <- sync_data %>% 
-  dplyr::filter(rmse < fltr_rmse50)  %>% 
+p4 <- sync_data %>%
+  dplyr::filter(rmse < fltr_rmse50)  %>%
   ggplot(aes(x = rmse)) +
   geom_density(outline.type = "full", fill = cbbPalette[3], alpha = 0.6) +
   labs(title = "RMSE for Sync and Ref tags (filtered by 50% quantile)", subtitle = paste(unique(sync_data$site))) +
@@ -109,13 +119,13 @@ ggsave(p4, file = "./output/plots/RMSE_q50_by_month.png", width = 8, height = 8)
 
 
 #' HPE vs HPEm
-# 
+#
 
-p5 <- sync_data %>% 
-  dplyr::filter(hpe < fltr_hpe95 & rmse < fltr_rmse50 & hpem < 1)  %>% 
+p5 <- sync_data %>%
+  dplyr::filter(hpe < fltr_hpe95 & rmse < fltr_rmse50 & hpem < 1)  %>%
   ggplot(aes(x = hpe, y = hpem)) +
   geom_point(aes(colour = hpe), alpha = 0.5) +
-  geom_smooth(method = "lm", formula = y~x, se = FALSE, colour = "black") + 
+  geom_smooth(method = "lm", formula = y~x, se = FALSE, colour = "black") +
   labs(title = "HPE vs HPEm (filtered by 50% quantile of HPE and RMSE)", subtitle = paste(unique(sync_data$site))) +
   facet_wrap(~stn) +
   theme_bw()
@@ -136,9 +146,9 @@ ggsave(p5, file = "./output/plots/HPE_vsHPEm_by_station.png", width = 8, height 
 glimpse(sync_data)
 
 #' **Optionally, can filter 1-month of data here to speed up processing - commented filter line**
-sync_data2 <- sync_data |> 
-  filter(hpe < fltr_hpe95 & rmse < fltr_rmse50) |> 
-  arrange(id, t) 
+sync_data2 <- sync_data |>
+  filter(hpe < fltr_hpe95 & rmse < fltr_rmse50) |>
+  arrange(id, t)
   # filter(t > "2022-08-01 00:00:00" & t < "2022-08-31 23:59:59") # select only 1 month of data to speed up trying things out
 
 glimpse(sync_data2)
@@ -149,8 +159,8 @@ glimpse(sync_data2)
 # Create a median positions object for plotting
 # =====================
 #' And a summary name file in order to plot position names and verify labelling:
-stn_med <- sync_data2 |> 
-  group_by(site, stn) |> 
+stn_med <- sync_data2 |>
+  group_by(site, stn) |>
   reframe(stn = unique(stn), pos = unique(pos), med_lat = median(lat, na.rm = TRUE), med_lon = median(lon, na.rm = TRUE))
 
 stn_med2 <- vect(stn_med, geom = c("med_lon", "med_lat"), crs = "EPSG:4326")
@@ -175,13 +185,23 @@ sync_data3_utm <- terra::project(sync_data3, crs_wgs84_utm19N)
 sync_data3_utm_sf <- st_as_sf(sync_data3_utm)
 
 
+# Transform receivers metadata into spatial objects
+rece_pilotes2 <- vect(rece_pilotes, geom = c("lon", "lat"), crs = "EPSG:4326")
+rece_pilotes2_utm <- terra::project(rece_pilotes2, crs_wgs84_utm19N)
+rece_pilotes2_utm_sf <- st_as_sf(rece_pilotes2_utm)
+
+vps_pilotes2 <- vect(vps_pilotes, geom = c("lon", "lat"), crs = "EPSG:4326")
+vps_pilotes2_utm <- terra::project(vps_pilotes2, crs_wgs84_utm19N)
+vps_pilotes2_utm_sf <- st_as_sf(vps_pilotes2_utm)
+colnames(vps_pilotes2_utm_sf)[5] <- 'stn'
+
 
 
 #' # Plot sync detections
 #' ----------------------------------------------------
 
 # =====================
-# Plot sync detections 
+# Plot sync detections
 # =====================
 
 #' Plot sync detections and median positions + labels
@@ -200,13 +220,110 @@ t1
 tmap_save(t1, file = "./output/plots/sync_detections.png")
 
 
+# Same graph than t1 but with ggplot and adding the receivers and VPS coordinates
+p6 <- sync_data3_utm_sf %>%
+  ggplot()+
+  geom_sf(col = "black")+
+  geom_sf(data=stn_med2_utm_sf, col = "red", cex = 1.0)+
+  geom_sf_text(data = stn_med2_utm_sf, aes(label = stn), col = "grey") +
+  geom_sf(data=rece_pilotes2_utm_sf, col ="blue") +
+  geom_sf(data=vps_pilotes2_utm_sf, col ="green") +
+  theme_bw()
+p6
+ggsave(p6, file = "./output/plots/sync_detections_PIL_receivers.png", width = 8, height = 8)
+
+
+
+# =====================
+# Calculate the difference-distance (in m) for each station with the median
+# =====================
+
+# Distance between the median and the GPS receivers coordinates
+diff_rece <- st_distance(stn_med2_utm_sf, rece_pilotes2_utm_sf, by_element = TRUE)
+
+# Converting the matrix in a dataframe
+diff_rece1 <- data.frame(diff_rece)
+names(diff_rece1)[1] <- "Diff_median_receivers"
+
+
+
+# Difference between the median and the VPS adjusted receivers coordinates
+diff_vps <- st_distance(stn_med2_utm_sf, vps_pilotes2_utm_sf, by_element = TRUE)
+
+# Converting the matrix in a dataframe
+diff_vps1 <- data.frame(diff_vps)
+names(diff_vps1)[1] <- "Diff_median_vps"
+
+
+
+# Difference between GPS receivers coordinates and VPS adjusted coordinates
+diff_rece_vps <- st_distance(rece_pilotes2_utm_sf, vps_pilotes2_utm_sf, by_element = TRUE)
+
+# Converting the matrix in a dataframe
+diff_rece_vps1 <- data.frame(diff_rece_vps)
+names(diff_rece_vps1)[1] <- "Diff_receivers_vps"
+
+
+
+# Combine the three dataframes
+diff <- cbind(diff_rece1, diff_vps1, diff_rece_vps1)
+diff
+
+# Add a Station column
+stn <- data.frame("stn" = c("Ref01", "Ref02", "St01", "St02", "St03", "St04", "St05", "St06", "St07", "St08", "St09"))
+diff1 <- cbind(stn, diff)
+diff1
+
+
+
+# =====================
+# Calculate MCP pour each station
+# =====================
+
+# Calculate the maximum density for each station
+# Not completely sure what I'm doing here, but it seems to work - comment from MFL
+p7 <- ggplot(data = sync_data3_utm_sf) +
+  geom_sf() +
+  theme_bw() +
+  stat_density_2d(mapping = ggplot2::aes(x = purrr::map_dbl(geometry, ~.[1]),
+                                         y = purrr::map_dbl(geometry, ~.[2]),
+                                         fill = stat(density)),
+                  geom = 'tile',
+                  contour = FALSE,
+                  alpha = 0.5)
+p7
+ggsave(p7, file = "./output/plots/Density_plot_PIL.png", width = 8, height = 8)
+
+
+# Addind the median results and receivers coordinates on the graph
+p8 <- ggplot(data = sync_data3_utm_sf) +
+  geom_sf() +
+  theme_bw() +
+  stat_density_2d(mapping = ggplot2::aes(x = purrr::map_dbl(geometry, ~.[1]),
+                                         y = purrr::map_dbl(geometry, ~.[2]),
+                                         fill = stat(density)),
+                  geom = 'tile',
+                  contour = FALSE,
+                  alpha = 0.5) +
+  geom_sf(data=stn_med2_utm_sf, col = "red", cex = 1.0)+
+  geom_sf_text(data = stn_med2_utm_sf, aes(label = stn), col = "grey") +
+  geom_sf(data=rece_pilotes2_utm_sf, col ="blue") +
+  geom_sf(data=vps_pilotes2_utm_sf, col ="green")
+p8
+ggsave(p8, file = "./output/plots/Density_plot_PIL_median.png", width = 8, height = 8)
 
 
 
 
+# =====================
+# Save data
+# =====================
 
 #' Export file with median positions:
 write_csv(stn_med2_utm_sf, file = "./output/datasets/median_positions_utm_sf.csv")
+
+# Export file with distance between receivers and vps coodinates with the median
+write_csv(diff1, file = "./output/datasets/distance_receivers_median.csv")
 
 
 
@@ -233,12 +350,12 @@ write_csv(stn_med2_utm_sf, file = "./output/datasets/median_positions_utm_sf.csv
 
 # # calculate median positions based on filtered (HPE less than 2 and RMSE less than 1) dataset
 
-# pos_summary <- sync_data %>% 
+# pos_summary <- sync_data %>%
 #   dplyr::filter(HPE < 2) %>%
-#   dplyr::filter(RMSE < 1) %>% 
-#   group_by(period, ID) %>% 
+#   dplyr::filter(RMSE < 1) %>%
+#   group_by(period, ID) %>%
 #   summarize(Stn = unique(Stn.y), Stn2 = unique(Stn.x), period = unique(period), Time_in = unique(Time_in), Time_out = unique(Time_out),
-#    med_lat = median(Lat), med_y = median(Y), med_lon = median(Lon), med_x = median(X), 
+#    med_lat = median(Lat), med_y = median(Y), med_lon = median(Lon), med_x = median(X),
 #     gps_lat = unique(gps_lat), gps_long = unique(gps_lon), gps_error = unique(gps_error), depth_mllw = unique(depth_mllw))
 
 
@@ -253,15 +370,15 @@ write_csv(stn_med2_utm_sf, file = "./output/datasets/median_positions_utm_sf.csv
 # #' Plot the vps and gps positions to visualize:
 # shapes <- c("GPS" = 16, "VPS" = 18)
 
-# pos_summary %>% 
+# pos_summary %>%
 #   ggplot(aes(x = med_lon, y = med_lat, colour = period)) +
-#   geom_point(aes(shape = "VPS"), size = 3, alpha = 0.5) + 
+#   geom_point(aes(shape = "VPS"), size = 3, alpha = 0.5) +
 #   geom_point(aes(x = gps_long, y = gps_lat, shape = "GPS"), size = 3, alpha = 0.5) +
 #   theme_bw() +
 #   geom_label_repel(aes(label = Stn2), fill = "white", show_guide = F) +
 #   scale_shape_manual(values = shapes) +
 #   scale_colour_manual(values = cbbPalette[2:3], ) +
-#   guides(colour = guide_legend("Period", override.aes = list(label = "", alpha = 1, size = 4)), 
+#   guides(colour = guide_legend("Period", override.aes = list(label = "", alpha = 1, size = 4)),
 #   	shape = guide_legend("GPS vs VPS"))
 
 # ggsave(file = "./R_output/plots/pos_summary_positions.png", width = 12, height = 12)
@@ -276,13 +393,13 @@ write_csv(stn_med2_utm_sf, file = "./output/datasets/median_positions_utm_sf.csv
 # # =====================
 
 # #' panelled by period and with gps points overlaid
-# p2 <- sync_data %>% 
-#   dplyr::filter(HPE < 2) %>% 
+# p2 <- sync_data %>%
+#   dplyr::filter(HPE < 2) %>%
 #   ggplot(aes(x = Lon, y = Lat, colour = HPE)) +
 #   geom_point(alpha = 0.1) +
 #   geom_point(data = pos_summary, aes(x = med_lon, y = med_lat), colour = "green", shape = 18, size = 2, alpha = 0.9) +
 #   geom_point(data = pos_summary, aes(x = gps_long, y = gps_lat), colour = "red", shape = 16, size = 2, alpha = 0.9) +
-#   theme_bw() + 
+#   theme_bw() +
 #   facet_wrap(~period)
 
 # plot(p2)
@@ -301,7 +418,7 @@ write_csv(stn_med2_utm_sf, file = "./output/datasets/median_positions_utm_sf.csv
 # Section
 # =====================
 
-#' # Text to explain 
+#' # Text to explain
 #' --------------------------------------------------
 
 # sutitle/section text
