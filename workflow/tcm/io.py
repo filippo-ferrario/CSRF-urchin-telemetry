401b7166564aa9eb345d1340727f58c4dc8c3e12
Paul Covert <Paul.Covert@dfo-mpo.gc.ca>
2025-02-14 13:45:40 -0500

    formal i/o functions

--- workflow/tcm/io.py
+++ workflow/tcm/io.py
@@ -4,7 +4,7 @@
   Name   	: io.py
   Author 	: Paul Covert
   Date   	: 2025-02-14
-  Version	: 1.0.0a
+  Version	: 1.0.0
   URL		: 
   Aim    	: Input / Output routines for TCM files
   ===============================================================================
@@ -14,8 +14,50 @@ import pandas as pd
 
 
 def read_csv(filepath_or_buffer):
-    df = pd.read_csv(filepath_or_buffer)
-    df["ISO 8601 Time"] = pd.to_datetime(df["ISO 8601 Time"])
-    df.set_index("ISO 8601 Time", inplace=True)
-    return df
+    """Read tilt current meter (TCM) csv files.
     
+    Both current and temperature files are supported.  For ease of
+    readability, the column names are renamed.  Speed and velocity, 
+    which are in units of cm/s in the TCM current files, are converted
+    to m/s.
+    
+    Parameters
+    ----------
+    filepath_or_buffer : str, path object or file-like object
+    
+    Returns
+    -------
+    df : time-indexed pandas DataFrame
+        Water current DataFrame contains ``time``, ``speed``, ``dir``, 
+        vel_N``, and ``vel_E`` columns.  Water temperature DataFrame 
+        contains ``time`` and ``temp`` columns.
+            
+    """
+    df = pd.read_csv(filepath_or_buffer)
+    if df.columns.values[1] == "Speed (cm/s)":
+        df.rename(
+            columns={
+                "ISO 8601 Time": "time",
+                "Speed (cm/s)": "speed",
+                "Heading (degrees)": "dir",
+                "Velocity-N (cm/s)": "velocity_north",
+                "Velocity-E (cm/s)": "velocity_east",
+            },
+            inplace=True
+        )
+        df["speed"] = df["speed"] / 100.
+        df["velocity_north"] = df["velocity_north"] / 100.
+        df["velocity_east"] = df["velocity_east"] / 100.
+    elif df.columns.values[1] == "Temperature (C)":
+        df.rename(
+            columns={
+                "ISO 8601 Time": "time",
+                "Temperature (C)": "xducer_temp",
+            },
+            inplace=True
+        )
+    else:
+        pass
+    df["time"] = pd.to_datetime(df["time"])
+    df.set_index("time", inplace=True)
+    return df
