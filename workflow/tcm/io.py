9aa7a3475e4bdd9ba14c34715755bedde98bdf23
Paul Covert <Paul.Covert@dfo-mpo.gc.ca>
2025-02-15 08:38:15 -0500

    Update io.py

--- workflow/tcm/io.py
+++ workflow/tcm/io.py
@@ -15,23 +15,23 @@ import pandas as pd
 
 def read_csv(filepath_or_buffer):
     """Read tilt current meter (TCM) csv files.
-    
+
     Both current and temperature files are supported.  For ease of
-    readability, the column names are renamed.  Speed and velocity, 
+    readability, the column names are renamed.  Speed and velocity,
     which are in units of cm/s in the TCM current files, are converted
     to m/s.
-    
+
     Parameters
     ----------
     filepath_or_buffer : str, path object or file-like object
-    
+
     Returns
     -------
     df : time-indexed pandas DataFrame
-        Water current DataFrame contains ``time``, ``speed``, ``dir``, 
-        vel_N``, and ``vel_E`` columns.  Water temperature DataFrame 
+        Water current DataFrame contains ``time``, ``speed``, ``dir``,
+        vel_N``, and ``vel_E`` columns.  Water temperature DataFrame
         contains ``time`` and ``temp`` columns.
-            
+
     """
     df = pd.read_csv(filepath_or_buffer)
     if df.columns.values[1] == "Speed (cm/s)":
@@ -43,18 +43,18 @@ def read_csv(filepath_or_buffer):
                 "Velocity-N (cm/s)": "velocity_north",
                 "Velocity-E (cm/s)": "velocity_east",
             },
-            inplace=True
+            inplace=True,
         )
-        df["speed"] = df["speed"] / 100.
-        df["velocity_north"] = df["velocity_north"] / 100.
-        df["velocity_east"] = df["velocity_east"] / 100.
+        df["speed"] = df["speed"] / 100.0
+        df["velocity_north"] = df["velocity_north"] / 100.0
+        df["velocity_east"] = df["velocity_east"] / 100.0
     elif df.columns.values[1] == "Temperature (C)":
         df.rename(
             columns={
                 "ISO 8601 Time": "time",
                 "Temperature (C)": "xducer_temp",
             },
-            inplace=True
+            inplace=True,
         )
     else:
         pass
